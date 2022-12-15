a691023310841e3b500be1b70a022f067ea002c9
Willian Coelho da Cunha <willian_comp@outlook.com>
2022-12-15 04:44:38 -0300

    feat: replace common accented characters

--- src/main/no-punctuation.ts
+++ src/main/no-punctuation.ts
@@ -1,5 +1,29 @@
 export class NoPunctuation {
 
+  private readonly ptBR: { [ key: string ]: string[] } = {
+    a: ['à', 'á', 'â', 'ã', 'ä', 'å'],
+    A: ['À', 'Á', 'Â', 'Ã', 'Ä', 'Å'],
+    c: ['ç'],
+    C: ['Ç'],
+    e: ['è', 'é', 'ê', 'ë'],
+    E: ['È', 'É', 'Ê', 'Ë'],
+    i: ['ì', 'í', 'î', 'ï'],
+    I: ['Ì', 'Í', 'Î', 'Ï'],
+    n: ['ñ'],
+    N: ['Ñ'],
+    o: ['ò', 'ó', 'ô', 'õ', 'ö', 'ø'],
+    O: ['Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø'],
+    s: ['š'],
+    S: ['Š'],
+    t: ['þ'],
+    u: ['ù', 'ú', 'û', 'ü'],
+    U: ['Ù', 'Ú', 'Û', 'Ü'],
+    y: ['ý', 'ÿ'],
+    Y: ['Ý', 'Ÿ'],
+    z: ['ž'],
+    Z: ['Ž']
+  };
+
   private string = '';
   
   constructor(string: string) {
@@ -8,6 +32,17 @@ export class NoPunctuation {
 
   build(): string {
     return this.string;
+  }
+
+  replaceCommonAccentedCharacters(): NoPunctuation {
+    for (const key in this.ptBR) {
+      const source: string[] = this.ptBR[key];
+
+      for (let i = 0; i < source.length; i++) {
+        this.string = this.string.replaceAll(source[i], key);
+      }
+    }
+    return this;
   }
 
   replaceManyWhiteSpacesTogetherForOnlyOne(): NoPunctuation {
--- src/test/no-punctuation.spec.ts
+++ src/test/no-punctuation.spec.ts
@@ -2,6 +2,13 @@ import { NoPunctuation } from '../main/no-punctuation';
 
 describe('NoPunctuation', () => {
 
+  it('should replace common accented characters for their respective non accented version.', () => {
+    expect(new NoPunctuation('A peça foi fantástica.').replaceCommonAccentedCharacters().build()).toEqual('A peca foi fantastica.');
+    expect(new NoPunctuation('O trânsito não melhora.').replaceCommonAccentedCharacters().build()).toEqual('O transito nao melhora.');
+    expect(new NoPunctuation('O tráfego aéreo é intenso.').replaceCommonAccentedCharacters().build()).toEqual('O trafego aereo e intenso.');
+    expect(new NoPunctuation('Observe esses exemplos: Acórdão; Bênção; Órfão; Órgãos; Sótão.').replaceCommonAccentedCharacters().build()).toEqual('Observe esses exemplos: Acordao; Bencao; Orfao; Orgaos; Sotao.');
+  });
+
   it('should identify more than one white spaces together and replace them for only one white space.', () => {
     expect(
       new NoPunctuation('This  is   a unit     test  !')
